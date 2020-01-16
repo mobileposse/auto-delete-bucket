9588cf81c2b66d1a26996a4cb6acd517a187932d
Jon Monroe <jon@outschool.com>
2020-01-15 22:13:52 -0800

    Add versioned bucket support for empty-bucket.js

--- src/lambda/empty-bucket.js
+++ src/lambda/empty-bucket.js
@@ -4,17 +4,19 @@ const s3 = new AWS.S3()
 
 export const emptyBucket = async bucketName => {
   // delete bucket contents
-  const listedObjects = await s3.listObjectsV2({ Bucket: bucketName }).promise()
+  const listedObjects = await s3
+    .listObjectVersions({ Bucket: bucketName })
+    .promise()
 
   // nothing left - we're done!
-  const contents = listedObjects.Contents || []
+  const contents = listedObjects.Versions || []
   if (contents.length === 0) return
 
   let records = []
 
   // make a list of objects to delete
   for (let record of contents) {
-    records.push({ Key: record.Key })
+    records.push({ Key: record.Key, VersionId: record.VersionId })
   }
 
   await s3
